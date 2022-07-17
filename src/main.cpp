0a72e49fe3329c10976e0d06be4db234ecd0af9d
kienphan301 <kienphan30103@gmail.com>
2022-07-17 12:58:23 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,28 @@
 #include <Arduino.h>
 
-byte led = 9;
-float val = 0.0;
-float state = 0.0;
+byte trig = 8;     // chân trig của HC-SR04
+byte echo = 9;     // chân echo của HC-SR04
 
-void print_sensor_value(){
-  Serial.print("val: ");
-  Serial.println(val);
-  delay(200);
+unsigned long duration; // biến đo thời gian
+float distance;         // biến lưu khoảng cách
+ 
+void setup(){
+    Serial.begin(9600);     // giao tiếp Serial với baudrate 9600
+    pinMode(trig, OUTPUT);   // chân trig sẽ phát tín hiệu
+    pinMode(echo, INPUT);    // chân echo sẽ nhận tín hiệu
 }
-
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(led, OUTPUT);
-  Serial.begin(9600);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  val = analogRead(A5);
-
-  state = 0.24927*val;
-  /*Serial.print("state: ");
-  Serial.println((int)state);
-
-  print_sensor_value();*/
-  
-  analogWrite(led, (int)state);
-}
+ 
+void loop(){
+    digitalWrite(trig, LOW);   // tắt chân trig
+    delayMicroseconds(2);
+    digitalWrite(trig, HIGH);   // phát xung từ chân trig
+    delayMicroseconds(5);   // xung có độ dài 5 microSeconds
+    digitalWrite(trig, LOW);   // tắt chân trig
+   
+    duration = pulseIn(echo, HIGH); 
+    distance = duration/2/29.412;
+    
+    Serial.print(distance);
+    Serial.println(" cm");
+    delay(100);
+} 
